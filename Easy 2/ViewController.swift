d81141cf8746620813ed5a2ebba4dd4f3e21dfad
kanesorn <joeey_wk@hotmail.com>
2016-06-23 15:05:14 +0700

    Initial Commit

---
+++ Easy 2/ViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  Easy 2
+//
+//  Created by iMac_13 on 6/23/2559 BE.
+//  Copyright © 2559 iMac_13. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func loginButton(sender: AnyObject) {
+        
+        let user = userTextField.text
+        let password = passwordTextField.text
+        
+        print("user ==> \(user)")
+        print("password ==>\(password)")
+        checkSpace(user!, strPass: password!)
+    }//login Button
+    
+    func checkSpace(strUser:String, strPass:String) -> Void
+    {
+    
+        if ((strUser.isEmpty) || (strPass.isEmpty)) {
+            
+            print("Have Space")
+            
+            myAlertDialog("Have Space", strMessage: "Please Fill All Bank")
+            
+            
+            
+            
+        } else {
+            var trueUser = ["test1","test2","test3"]
+            var truePassword = ["123","456","789"]
+            var index = 0
+            var status = false
+            var myTruePass = ""
+            
+            
+            for myFor in trueUser {
+                if (strUser == myFor) {
+                    status = true
+                    myTruePass = truePass[index]
+                    
+                }//if
+                index += 1
+                
+            }//for
+            
+            if status {
+                <#code#>
+            }else{
+            
+            }
+            
+        }
+        
+    }
+    
+    func myAlertDialog(strTit:String, strMessage:String) -> Void {
+        //Alert Dialog
+        let myAlert = UIAlertController(title: strTit ,message: strMessage ,preferredStyle: UIAlertControllerStyle.Alert)
+        myAlert.addAction(UIAlertAction(title:"OK",style: UIAlertActionStyle.Default,handler:nil))
+        self.presentViewController(myAlert, animated: true, completion: nil)
+    
+    }//Alert func
+    
+}//class
+
